e27185b85b973413e237c4283e4a83579f8c95c3
TeliStaffs <109075511+TeliAccenture@users.noreply.github.com>
2022-07-26 10:44:11 +0100

    Added fake dataset
    WIP dynamic store functionality

---
+++ E-WasteSite-main/JS/dataSet.js
@@ -0,0 +1,40 @@
+let productsArray = [{
+    "productTitle": "Lightbulbs",
+    "productPrice": "£50",
+    "productImage": "../images/Lightbulbs.jpg",
+    "productDescription" : "Lorem ipsum"
+},
+{
+    "productTitle": "Keyboard",
+    "productPrice": "£10",
+    "productImage": "../images/NatureKeyboard.jpg",
+    "productDescription" : "Lorem ipsum"
+
+},
+{
+    "productTitle": "TV",
+    "productPrice": "£30",
+    "productImage": "../images/TVinNature.jpg",
+    "productDescription" : "Lorem ipsum"
+
+}
+]
+
+let usersArray = [{
+    "username": "Joanna",
+    "email": "joanna@gmail.com",
+    "password": "qweQWE123!"
+},
+{
+    "username": "Telisa",
+    "email": "telisa@gmail.com",
+    "password": "qweQWE123!"
+},
+{
+    "username": "Emily",
+    "email": "Emily@gmail.com",
+    "password": "qweQWE123!"
+}
+]
+
+export {productsArray, usersArray};
--- E-WasteSite-main/JS/productCard.js
+++ E-WasteSite-main/JS/productCard.js
@@ -1,28 +1,31 @@
 //Credited to Telisa DP, Accenture Internship, Code First Girls 2022
 
+import {productsArray} from "./dataSet"
+
 let html = '';
-let products;
 let oldelem;
 let newelem;
-    fetch("./Products.JSON").then(response => response.json()).then(data => console.log(data))
-    
-    
-    .then(
-    JSON.parse(products).forEach(product => {
-        let htmlSegment = `<div id = "productCard" class = "productCard">
-        <img src = "${product.productImage}" class="productImg">
-        <h4 class="cardTitle"> ${product.productName}</h4>
-        <h4 class="cardTitle"> ${product.productPrice}</h4>
-        </div>`;
+
+function populateProducts(){
+    productsArray.forEach(product => {
+        let htmlSegment = `<div id = "product" class="productCard">
+        <h2 class="formTitle">${product.productImage}</h2>
+        <img src = "${product.productImage}" alt = "product image" height = "250px" width = "250px">
+        <p id = "productDescription" class="formText" >${product.productDescription}</p>
+        <p id = "price" class="formText">${product.productPrice}</p>
+        <button class="normalButton">Add to Cart</button>
+    </div>`;
 
         html += htmlSegment;
-    })    
-    ).then(
- oldelem = document.querySelector("script#productContainer") )
-    .then( newelem = document.createElement("div"))
-    .then(newelem.innerHTML = htmlSegment)
-    .then(oldelem.parentNode.replaceChild(newelem,oldelem))
-       
+    })
+newelem = document.querySelector("#productGallery");
+newelem.innerHTML = htmlSegment;
+console.log(htmlSegment)
+}
+
+populateProducts();
+
+
  
 
 
